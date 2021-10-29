5b62dad380cabbe8baa260688b4fe7ca878b5c25
Oskar <oskar.madej.wroc@gmail.com>
2021-10-29 04:50:35 +0200

    +navigation

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_134334) do
+ActiveRecord::Schema.define(version: 2021_10_29_021819) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.datetime "date"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.date "date"
   end
 
   create_table "users", force: :cascade do |t|
